2726b38179fcfb2cc64cf8283229224b550769cd
Ihor Kurnytskyi <ihor.kurnytskyi@cz.abb.com>
2019-11-06 08:11:15 +0100

    create pages

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby';
+
+export default () => (
+    <div>
+      Sorry we couldn't find this page. 
+      <div>
+        <Link to='/'>Go home</Link>
+      </div>
+    </div>
+  )
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import { Link } from "gatsby";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Link to='/'>Main page</Link> | <Link to="/about">About</Link>
+    <h1>This is Main page</h1>
+  </div>
+)
