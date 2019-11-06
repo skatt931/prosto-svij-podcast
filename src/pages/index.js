5b7b8a4c498e3307213ca3f15c0e737f8f51e637
Ihor Kurnytskyi <ihor.kurnytskyi@cz.abb.com>
2019-11-06 08:22:29 +0100

    text add

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,8 @@ export default () => (
   <div>
     <Link to='/'>Main page</Link> | <Link to="/about">About</Link>
     <h1>This is Main page</h1>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing.
+    </p>
   </div>
 )
